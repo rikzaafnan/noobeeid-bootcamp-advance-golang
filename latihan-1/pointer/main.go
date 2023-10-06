32e8f8e53b79d876be0b0f0ecd141676932b8801
afnan <afnan@fastratabuana.co.id>
2023-10-06 09:46:11 +0700

    tugas 2

---
+++ latihan-1/pointer/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	dataStudent := Student{
+		Name:  "carly",
+		Class: 1,
+	}
+
+	dataStudent.CallMyName()
+
+}
+
+type Student struct {
+	Name  string
+	Class int
+}
+
+type StudentInterface interface {
+	SetMyName(name string)
+	CallMyName()
+}
+
+func (s Student) SetMyName(name string) {
+	s.Name = name
+}
+
+func (s Student) CallMyName() {
+
+	fmt.Println("Hello, My Name is", s.Name)
+
+}

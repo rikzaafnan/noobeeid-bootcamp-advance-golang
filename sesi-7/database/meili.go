b9bf2f4f64edc24e7c3e5c8c687a4bc18f8115ce
rikzaafnan <rikzaafnan@gmail.com>
2023-11-17 18:59:44 +0700

    add meili

--- sesi-7/database/meili.go
+++ sesi-7/database/meili.go
@@ -1,1 +1,21 @@
 package database
+
+import (
+	"errors"
+	"github.com/meilisearch/meilisearch-go"
+)
+
+func ConnectMeilisearch() (client *meilisearch.Client, err error) {
+	// init meilisearch client
+	client = meilisearch.NewClient(meilisearch.ClientConfig{
+		Host:   "http://localhost:7700", // setup meilisearch host
+		APIKey: "ThisIsMasterKey",       // setup meilisearch api key
+	})
+
+	// validate is client null or not
+	if client == nil {
+		return nil, errors.New("error when try to connect to meilisearch")
+	}
+
+	return
+}

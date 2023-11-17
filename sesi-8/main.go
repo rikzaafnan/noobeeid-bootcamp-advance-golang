dd7064ccf2e598267c2939a89df50a1a820e7c33
afnan <afnan@fastratabuana.co.id>
2023-11-17 14:43:13 +0700

    add sesi-8

--- sesi-8/main.go
+++ sesi-8/main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"log"
+	"os"
 	"sesi-8/config"
 )
 
+var path string
+
 func main() {
 
-	err := config.LoadConfig("./.env")
+	err := config.LoadConfig(".env")
 	if err != nil {
 		log.Println("no file .env")
 	}
 
 	appPort := config.GetConfigString("APP_PORT", ":4444")
+	path = config.GetConfigString("PATH", "public/uploads")
 
 	router := fiber.New()
 
@@ -28,6 +32,20 @@ func main() {
 }
 
 func Upload(c *fiber.Ctx) error {
+
+	file, err := c.FormFile("file")
+	if err != nil {
+		log.Println("error when try to parse FormFile with detail : ", err.Error())
+	}
+
+	if file.Size > 1*1024*1024 {
+		errBadRequest := fiber.ErrBadRequest
+		errBadRequest.Message = "file to big, expected 1 Mb"
+		return errBadRequest
+	}
+
+	os.Create(path + "/" + file.Filename)
+
 	return nil
 }
 
